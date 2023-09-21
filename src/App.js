45b02be96db77e41b9fa2b38fa052dd273bf222a
Michal Kurek (Mike) <michal@kurek.pl>
2023-09-21 14:27:11 +0200

    add styles to app.css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,38 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>it works</h1>
+    <div className="app">
+      <h1>To-do List</h1>
+      <form>
+        <label
+          htmlFor="text"
+          className="text-label"
+        >
+          Enter task
+          <input
+            className="text-input"
+            type="text"
+            id="text"
+          />
+        </label>
+        <button className="task-button">Add Task</button>
+      </form>
+      <p>Tasks:</p>
+      <ul>
+        <li>
+          <label
+            htmlFor=""
+            className="task-label"
+          >
+            <input
+              type="checkbox"
+              className="checkbox-input"
+            />
+            Task
+          </label>
+          <button className="delete-button">Delete</button>
+        </li>
+      </ul>
     </div>
   );
 }
